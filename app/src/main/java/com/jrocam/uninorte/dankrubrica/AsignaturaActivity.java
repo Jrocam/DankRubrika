6e18e573ef6213eb1339818ff8d79a9a3e3f43c4
Juan Roca <juanmaa13@gmail.com>
2017-09-08 15:43:28 -0500

    Third Dank

--- app/src/main/java/com/jrocam/uninorte/dankrubrica/AsignaturaActivity.java
+++ app/src/main/java/com/jrocam/uninorte/dankrubrica/AsignaturaActivity.java
@@ -1,6 +1,7 @@
 package com.jrocam.uninorte.dankrubrica;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,16 +9,30 @@ import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Map;
 
 public class AsignaturaActivity extends MainActivity implements NavigationView.OnNavigationItemSelectedListener {
     private LinearLayout parentLinearLayout;
     private int count=0;
     private DrawerLayout mDraverHijo;
+    private DatabaseReference myRef;
+    private TextView asignaturaText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +58,11 @@ public class AsignaturaActivity extends MainActivity implements NavigationView.O
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        navigationView.getMenu().getItem(1).setChecked(true);
+        //FIREBASE
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        myRef = database.getReference();
+        setAsignaturas();
     }
 
     public void onAsignatura(View view) {
@@ -52,17 +72,74 @@ public class AsignaturaActivity extends MainActivity implements NavigationView.O
     }
 
     private void onAddField(View v){
-        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        final View rowView = inflater.inflate(R.layout.list_asignatura_card, null);
-        TextView texto = (TextView) rowView.findViewById(R.id.titulo_asignatura);
-        count++;
-        texto.setText("ASIGNATURA: "+count);
+        //crea un dialogo y lo muestra en pantalla
+        showInputDialog();
 
-        parentLinearLayout.addView(rowView, parentLinearLayout.getChildCount() - count);
+        //añade al layout la nueva asignatura si apretó OK
+
     }
     public void onDelete(View v){
         parentLinearLayout.removeView((View) v.getParent());
         count--;
+    }
+    public void setAsignaturas(){
+        //Constant Data retriever from firebase
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                //many stuff, obteniendo materias de un user
+                for (DataSnapshot postSnapshot: dataSnapshot.child("users").child("test").child("class").getChildren()) {
+                    // TODO: handle the post
+                    Map<String, Object> v = (Map<String, Object>) postSnapshot.getValue();
+
+                    //Put shit on components. yea boi. that's right
+                    Log.d("Msg", "VALUUEEEE is: " + v.get("name"));
+                    LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                    final View rowView = inflater.inflate(R.layout.list_asignatura_card, null);
+                    TextView texto = (TextView) rowView.findViewById(R.id.titulo_asignatura);
+                    count++;
+                    texto.setText(v.get("name").toString());
+                    parentLinearLayout.addView(rowView, parentLinearLayout.getChildCount() - count);                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.d("Msg", "Failed to read value.", error.toException());
+            }
+        });
+    }
+    protected void showInputDialog() {
+
+        // get prompts.xml view
+        LayoutInflater layoutInflater = LayoutInflater.from(AsignaturaActivity.this);
+        View promptView = layoutInflater.inflate(R.layout.input_asignatura, null);
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(AsignaturaActivity.this);
+        alertDialogBuilder.setView(promptView);
+
+        final EditText editText = (EditText) promptView.findViewById(R.id.edittext);
+        // setup a dialog window
+        alertDialogBuilder.setCancelable(false)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                        final View rowView = inflater.inflate(R.layout.list_asignatura_card, null);
+                        asignaturaText = (TextView) rowView.findViewById(R.id.titulo_asignatura);
+                        count++;
+                        asignaturaText.setText(editText.getText());
+                        parentLinearLayout.addView(rowView, parentLinearLayout.getChildCount() - count);
+                    }
+                })
+                .setNegativeButton("Cancelar",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+
+        // create an alert dialog
+        AlertDialog alert = alertDialogBuilder.create();
+        alert.show();
     }
 
 }
--- app/src/main/java/com/jrocam/uninorte/dankrubrica/RubricaActivity.java
+++ app/src/main/java/com/jrocam/uninorte/dankrubrica/RubricaActivity.java
@@ -8,16 +8,26 @@ import android.support.design.widget.Snackbar;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Map;
 
 public class RubricaActivity extends MainActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private LinearLayout parentLinearLayout;
     private int count=0;
     private DrawerLayout mDraverHijo;
+    private DatabaseReference myRef;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,7 +45,7 @@ public class RubricaActivity extends MainActivity implements NavigationView.OnNa
                         .setAction("Action", null).show();
             }
         });
-        /*mDraverHijo = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDraverHijo = (DrawerLayout) findViewById(R.id.drawer_layout);
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this,  mDraverHijo, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -43,8 +53,12 @@ public class RubricaActivity extends MainActivity implements NavigationView.OnNa
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        sunavigationView.setNavigationItemSelectedListener(this);*/
-
+        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.getMenu().getItem(0).setChecked(true);
+        //FIREBASE
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        myRef = database.getReference();
+        setRubrics();
     }
 
 
@@ -53,12 +67,38 @@ public class RubricaActivity extends MainActivity implements NavigationView.OnNa
         final View rowView = inflater.inflate(R.layout.list_rubrica_card, null);
         TextView texto = (TextView) rowView.findViewById(R.id.titulo_rubrica);
         count++;
-        texto.setText("RUBRICA: "+count);
-
+        texto.setText("RUBRICA: "+ count);
         parentLinearLayout.addView(rowView, parentLinearLayout.getChildCount() - count);
     }
     public void onDelete(View v){
         parentLinearLayout.removeView((View) v.getParent());
         count--;
+    }
+    public void setRubrics(){
+        //Constant Data retriever from firebase
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                //many stuff, obteniendo materias de un user
+                for (DataSnapshot postSnapshot: dataSnapshot.child("users").child("test").child("rubrics").getChildren()) {
+                    // TODO: handle the post
+                    Map<String, Object> v = (Map<String, Object>) postSnapshot.getValue();
+
+                    //Put shit on components. yea boi. that's right
+                    Log.d("Msg", "VALUUEEEE is: " + v.get("name"));
+                    LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                    final View rowView = inflater.inflate(R.layout.list_rubrica_card, null);
+                    TextView texto = (TextView) rowView.findViewById(R.id.titulo_rubrica);
+                    count++;
+                    texto.setText(v.get("name").toString());
+                    parentLinearLayout.addView(rowView, parentLinearLayout.getChildCount() - count);                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.d("Msg", "Failed to read value.", error.toException());
+            }
+        });
     }
 }
